b72d618d7e7c55f458e7e81219625f81074ff130
sagar <sagaredge11@gmail.com>
2022-11-15 18:09:13 +0530

    Sagar

--- assignment_pro/assignment_app/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Products
-
-
-class Prod_Serializ(ModelSerializer):
-    class Meta:
-         model = Products
-         fields = ['name','cost','stock_qty','volume']
